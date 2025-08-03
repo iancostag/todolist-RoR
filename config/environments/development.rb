7e4059270f70c69fa025d40202cdb03212e8c849
Ian Costa Guimarães <iancostag@outlook.com>
2025-08-03 01:35:38 -0300

    Teste

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,8 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
+  config.hosts << "todolist-ror.fly.dev"
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Make code changes take effect immediately without server restart.
